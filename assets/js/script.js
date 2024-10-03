29f6adcf9ecc2251cb3fdd7353383bf06422b01d
Baptiste Lecordier <91879838+BaptisteLECORDIER@users.noreply.github.com>
2024-10-03 16:45:04 +0200

    cart div work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,17 @@ console.log('JS OK')
 var blNavs = document.querySelectorAll('.bl-nav');
 
 blNavs.forEach(blNav => {
-    console.log('OK')
+
+    blNav.querySelector('.bl-nav-cart-btn').addEventListener("click", function () {
+        blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-default-state');
+        blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-close-state');
+        blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-open-state');
+
+    });
+
+    blNav.querySelector('.bl-nav-cart-exit-button').addEventListener("click", function () {
+        blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-open-state');
+        blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-close-state');
+
+    });
 });
