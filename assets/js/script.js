f82ba758456bb59b34711fc4079297fb9ff12edd
Baptiste Lecordier <91879838+BaptisteLECORDIER@users.noreply.github.com>
2024-10-04 17:23:10 +0200

    Cart in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,66 @@
 console.log('JS OK')
 
 
-var blNavs = document.querySelectorAll('.bl-nav');
 
-blNavs.forEach(blNav => {
+const blNav = document.querySelector('.bl-nav');
 
-    blNav.querySelector('.bl-nav-cart-btn').addEventListener("click", function () {
-        blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-default-state');
-        blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-close-state');
-        blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-open-state');
+function openCart() {
+    blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-default-state');
+    blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-close-state');
+    blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-open-state');
+    document.querySelector('.bl-body').classList.add('obscured-while-cart-is-open');
+    blNav.classList.add('obscured-while-cart-is-open');
+}
 
-    });
+function closeCart() {
+    blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-open-state');
+    blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-close-state');
+    document.querySelector('.bl-body').classList.remove('obscured-while-cart-is-open');
+    blNav.classList.remove('obscured-while-cart-is-open');
 
-    blNav.querySelector('.bl-nav-cart-exit-button').addEventListener("click", function () {
-        blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-open-state');
-        blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-close-state');
+}
 
-    });
-});
+
+
+
+
+
+    
+
+blNav.querySelector('.bl-nav-cart-btn').addEventListener("click", openCart);
+
+blNav.querySelector('.bl-nav-cart-exit-button').addEventListener("click", closeCart);
+    
+
+
+
+
+
+
+
+
+    
+
+// document.querySelectorAll('.bl-nav').forEach(blNav => {
+
+
+
+
+//     blNav.querySelector('.bl-nav-cart-btn').addEventListener("click", function openCart() {
+//         blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-default-state');
+//         blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-close-state');
+//         blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-open-state');
+//         document.querySelector('.bl-body').classList.add('obscured-while-cart-is-open');
+//         blNav.classList.add('obscured-while-cart-is-open');
+//     });
+
+//     blNav.querySelector('.bl-nav-cart-exit-button').addEventListener("click", function closeCart() {
+//         blNav.querySelector('.bl-nav-cart-content').classList.remove('bl-nav-cart-open-state');
+//         blNav.querySelector('.bl-nav-cart-content').classList.add('bl-nav-cart-close-state');
+//         document.querySelector('.bl-body').classList.remove('obscured-while-cart-is-open');
+//         blNav.classList.remove('obscured-while-cart-is-open');
+
+//     });
+// });
+
+
